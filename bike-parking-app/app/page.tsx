6bc954934117096820bb5ff005864dc6c01474a1
Christopher Lai <clai20707@gmail.com>
2024-03-09 21:09:38 -0500

    Render 'AccessScreen' on home page

--- bike-parking-app/app/page.tsx
+++ bike-parking-app/app/page.tsx
@@ -1,14 +1,18 @@
 "use client";
 import dynamic from "next/dynamic";
 import { Suspense } from "react";
+import AccessScreen from "./accessScreen/page";
 // import BikeMap from "./components/BikeMap";
-const DynamicMapComponent = dynamic(() => import("./components/LeafletMap"), {ssr: false});
+const DynamicMapComponent = dynamic(() => import("./components/LeafletMap"), {
+  ssr: false,
+});
 
 export default function Home() {
   return (
     <>
       {/* <BikeMap /> */}
-      <DynamicMapComponent />
+      {/* <DynamicMapComponent /> */}
+      <AccessScreen />
     </>
   );
 }
