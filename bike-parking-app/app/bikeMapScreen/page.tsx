f3c972848ecc0960a337fa785a1decd111ffb21d
Christopher Lai <clai20707@gmail.com>
2024-03-10 15:33:50 -0400

    Set up initial auth/access page

---
+++ bike-parking-app/app/bikeMapScreen/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+import dynamic from "next/dynamic";
+const DynamicMapComponent = dynamic(() => import("../components/LeafletMap"), {
+  ssr: false,
+});
+
+export default function page() {
+  return <DynamicMapComponent />;
+}
