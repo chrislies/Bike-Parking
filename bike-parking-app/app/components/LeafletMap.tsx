d24a5b4d3add724050e7b252bfbe4a59fcb499c5
Christopher Lai <clai20707@gmail.com>
2024-03-09 21:09:15 -0500

    Cleaned up code

--- bike-parking-app/app/components/LeafletMap.tsx
+++ bike-parking-app/app/components/LeafletMap.tsx
@@ -11,7 +11,6 @@ import "leaflet/dist/leaflet.css";
 // import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css";
 // import "leaflet-defaulticon-compatibility";
 import getCoordinates from "../lib/getCoordinates";
-import { FALSE } from "ol/functions";
 
 interface MarkerData {
   coordinates: [number, number];
