4a55c635599752a2fe46964b1fb6f9eb360ccbd8
chrislies <clai20707@gmail.com>
2024-03-10 20:46:05 -0400

    Updated 'access screen'

--- bike-parking-app/app/accessScreen/page.tsx
+++ bike-parking-app/app/accessScreen/page.tsx
@@ -2,10 +2,12 @@ import Link from "next/link";
 
 export default function AccessScreen() {
   return (
-    <div className="flex items-center justify-center text-center h-screen">
-      <div className="flex flex-col gap-5">
-        <h1>Sign In Page</h1>
-        <Link href="bikeMapScreen">Continue as Guest</Link>
+    <div>
+      <div className="flex items-center justify-center text-center h-screen">
+        <div className="flex flex-col gap-5">
+          <h1>Sign In Page</h1>
+          <Link href="bikeMapScreen">Continue as Guest</Link>
+        </div>
       </div>
     </div>
   );
