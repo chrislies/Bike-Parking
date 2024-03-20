37a4fcf4d47e11d281c4ce9f669390ba033f4ef2
Shion1314 <90737928+Shion1314@users.noreply.github.com>
2024-03-20 01:42:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -56,8 +56,8 @@ app.use(express.json());
 app.use(function(req,res,next){
   res.header("Access-Control-Allow-Origin","*");
   res.header("Access-Control-Allow-Methods","GET,POST,DELETE,PUT");
-  res.header("Access-Control-Allowo-Headers","Origin,X-Requested-With, Content-Type,Accepct,Authorization"
-  );
+res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
   next();
 })
 
