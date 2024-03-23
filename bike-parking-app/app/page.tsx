54c271be5cf695553d04597444079eb73131358b
Christopher Lai <clai20707@gmail.com>
2024-03-22 22:57:27 -0400

    Updated page file structure

--- bike-parking-app/app/page.tsx
+++ bike-parking-app/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import LoginModal from "@/components/auth/LoginModal";
-import AuthPage from "./(auth)/page";
-import AuthLayout from "./(auth)/layout";
+import AuthPage from "./auth/page";
+import AuthLayout from "./auth/layout";
 
 export default function Home() {
   return (
