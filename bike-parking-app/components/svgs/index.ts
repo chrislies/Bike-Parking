bf3e556c7b6ca00ff68f976c2c23c7d8e525b491
Christopher Lai <clai20707@gmail.com>
2024-03-27 16:15:03 -0400

    Include 'Spinner.svg' to export

--- bike-parking-app/components/svgs/index.ts
+++ bike-parking-app/components/svgs/index.ts
@@ -1,3 +1,4 @@
+export { default as Spinner } from "./Spinner.svg";
 export { default as Layers } from "./Layers.svg";
 export { default as Locate } from "./Locate.svg";
 export { default as MyMarker } from "./MyMarker.svg";
