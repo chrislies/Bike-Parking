642e02b3ffe64c460564dcf1a1037c4aa68575af
Christopher Lai <clai20707@gmail.com>
2024-03-27 19:24:54 -0400

    Setup user session

---
+++ bike-parking-app/components/User.tsx
@@ -0,0 +1,9 @@
+"use client";
+import { useSession } from "next-auth/react";
+
+const User = () => {
+  const { data: session } = useSession();
+  return <pre>{JSON.stringify(session)}</pre>;
+};
+
+export default User;
