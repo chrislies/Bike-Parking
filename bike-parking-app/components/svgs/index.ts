a4c4c653d72cfb774f0cb8e56223512ff50122bf
Christopher Lai <clai20707@gmail.com>
2024-03-27 23:40:18 -0400

    Include 'Bookmark.svg' to export

--- bike-parking-app/components/svgs/index.ts
+++ bike-parking-app/components/svgs/index.ts
@@ -1,5 +1,6 @@
 export { default as Spinner } from "./Spinner.svg";
 export { default as Layers } from "./Layers.svg";
 export { default as Locate } from "./Locate.svg";
+export { default as Bookmark } from "./Bookmark.svg";
 export { default as MyMarker } from "./MyMarker.svg";
 export { default as UserMarker } from "./UserMarker.svg";
