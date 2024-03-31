4b1045a92d5359ea49f020aa4fb6f89160ee6218
Christopher Lai <clai20707@gmail.com>
2024-03-31 16:22:30 -0400

    Setup navbar

--- bike-parking-app/components/navbar/Navbar.tsx
+++ bike-parking-app/components/navbar/Navbar.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { Add, Bookmark, Map, NavbarBookmark, User } from "../svgs";
+import { Add, Map, NavbarBookmark, User } from "../svgs";
+import Link from "next/link";
 
-const svgSize = 7;
+// const svgSize = 7;
+const svgSize = 6;
 
 const handleFavoritesClick = () => {};
 
@@ -36,16 +38,15 @@ const NAV_LINKS = [
 ];
 
 export default function Navbar() {
-  const router = useRouter();
+  // const router = useRouter();
   return (
     <div className="absolute bottom-3 z-[999] max-w-fit mx-auto flex inset-x-0 justify-center select-none rounded-2xl">
       <div className="grid grid-flow-col grid-cols-4 gap-1 w-[400px] bg-white rounded-2xl border-2 border-[rgba(0,0,0,0.2)] shadow-md p-1">
-        {NAV_LINKS.map((navItem) => (
-          <button
+        {NAV_LINKS.map((navItem, index) => (
+          <Link
+            key={index}
+            href={navItem.href}
             className="flex flex-col items-center justify-center py-1 hover:bg-gray-300 hover:rounded-xl"
-            onClick={() => {
-              router.push(navItem.href);
-            }}
           >
             <span>{navItem.svg}</span>
             <p
@@ -54,7 +55,7 @@ export default function Navbar() {
             >
               {navItem.label}
             </p>
-          </button>
+          </Link>
         ))}
       </div>
     </div>
