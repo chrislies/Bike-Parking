8d84ce26f3af91c089aecc8212c58a688552809b
Christopher Lai <clai20707@gmail.com>
2024-04-01 20:11:50 -0400

    Removed app

--- bike-parking-app/app/map/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import dynamic from "next/dynamic";
-const DynamicMapComponent = dynamic(() => import("@/components/LeafletMap"), {
-  ssr: false,
-});
-
-export default function page() {
-  return <DynamicMapComponent />;
-}
