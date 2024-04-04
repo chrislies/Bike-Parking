06215c20214f0b8f5f5011d1dc87c0ed6fc8a4ac
洪宣荣 <hongxuanrong@hongxuanrongdeMacBook-Pro.local>
2024-04-04 12:49:22 -0400

    temp marker

--- bike-parking-app/components/svgs/index.ts
+++ bike-parking-app/components/svgs/index.ts
@@ -12,3 +12,4 @@ export { default as FavoriteMarker } from "./FavoriteMarker.svg";
 export { default as SearchPin } from "./SearchPin.svg";
 export { default as Directions } from "./Directions.svg";
 export { default as NoImage } from "./NoImage.svg";
+export { default as TempMarker } from "./TempMarker.svg";
