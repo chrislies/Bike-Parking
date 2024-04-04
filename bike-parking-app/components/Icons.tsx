4d36e9f7a7af0996debbfccb24fa0053bcf32381
Christopher Lai <clai20707@gmail.com>
2024-04-04 00:05:26 -0400

    Move icons to single file

---
+++ bike-parking-app/components/Icons.tsx
@@ -0,0 +1,65 @@
+import L, {
+  LatLng,
+  map,
+  divIcon,
+  Icon,
+  point,
+  MarkerCluster,
+  Point,
+} from "leaflet";
+import ReactDOMServer from "react-dom/server";
+import {
+  Bookmark,
+  FavoriteMarker,
+  Layers,
+  Locate,
+  MyMarker,
+  UserMarker,
+  SearchPin,
+} from "./svgs";
+
+const customIcon = L.divIcon({
+  className: "custom-icon",
+  html: ReactDOMServer.renderToString(<MyMarker />),
+  // iconSize: [10, 10],
+  iconAnchor: [20, 30],
+  popupAnchor: [3, -16],
+});
+
+const favoriteIcon = L.divIcon({
+  className: "favorite-icon",
+  html: ReactDOMServer.renderToString(<FavoriteMarker />),
+  // iconSize: [10, 10],
+  iconAnchor: [20, 30],
+  popupAnchor: [3, -16],
+});
+
+const queryIcon = L.divIcon({
+  className: "query-icon",
+  html: ReactDOMServer.renderToString(<SearchPin />),
+  // iconSize: [10, 10],
+  iconAnchor: [20, 30],
+  popupAnchor: [3, -16],
+});
+
+const userIcon = L.divIcon({
+  className: "user-icon",
+  html: ReactDOMServer.renderToString(<UserMarker />),
+  iconSize: [18, 18],
+});
+
+const createClusterCustomIcon = function (cluster: MarkerCluster) {
+  return L.divIcon({
+    html: `<span class="cluster-icon">${cluster.getChildCount()}</span>`,
+    className: "custom-marker-cluster",
+    iconSize: point(33, 33, true),
+  });
+};
+
+export {
+  customIcon,
+  favoriteIcon,
+  userIcon,
+  queryIcon,
+  createClusterCustomIcon,
+};
