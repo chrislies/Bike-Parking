23f9f23364cec5effe970e983e21e28fe5026afa
Christopher Lai <clai20707@gmail.com>
2024-04-07 15:48:19 -0400

    Clean up code

--- bike-parking-app/app/page.tsx
+++ bike-parking-app/app/page.tsx
@@ -1,8 +1,5 @@
 import LoginModal from "@/components/auth/LoginModal";
 import AuthLayout from "./(entry)/layout";
-import { redirect } from "next/navigation";
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
 
 export default async function Home() {
   return (
@@ -11,10 +8,3 @@ export default async function Home() {
     </AuthLayout>
   );
 }
-
-// export default async function Home() {
-//   const supabase = createServerComponentClient({ cookies }); // Pages inside app/ are 'server' components by deufalt
-//   const { data: User } = await supabase.from("User").select();
-
-//   return <pre>{JSON.stringify(User, null, 2)}</pre>;
-// }
