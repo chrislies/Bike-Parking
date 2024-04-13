975528d92d3c9e25fb11c0294377099adc423d8f
em <80295156+em-cp@users.noreply.github.com>
2024-04-13 00:49:25 -0400

    Add files via upload

---
+++ bike-parking-app/app/(entry)/admin/dashboard/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import DashboardPage from "@/components/auth/DashboardPage"; // Adjust the import path as needed
+
+const AdminDashboardPage: React.FC = () => {
+  return <DashboardPage />;
+}
+
+export default AdminDashboardPage;
