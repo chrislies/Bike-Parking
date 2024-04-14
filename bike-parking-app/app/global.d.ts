f98c8572880f7d78f7a373ada728aeb202458ac5
Christopher Lai <clai20707@gmail.com>
2024-04-13 20:57:10 -0400

    Update and add global MarkerData interface

--- bike-parking-app/app/global.d.ts
+++ bike-parking-app/app/global.d.ts
@@ -2,4 +2,22 @@ import { Database as DB } from "@/lib/database.types";
 
 declare global {
   type Database = DB;
+
+  interface MarkerData {
+    x?: number;
+    y?: number;
+    id?: string;
+    address?: string;
+    rack_type?: string;
+    date_inst?: string;
+    sign_description?: string;
+    sign_code?: string;
+    favorite: boolean;
+    type: string;
+  }
+
+  interface UserCoordinatesItem {
+    longitude: number;
+    latitude: number;
+  }
 }
