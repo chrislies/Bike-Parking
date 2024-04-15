378a0b13ed7d96990d2ae4f7046707036e576919
em <80295156+em-cp@users.noreply.github.com>
2024-04-14 20:05:03 -0400

    Add files via upload

--- bike-parking-app/components/auth/LoginModal.tsx
+++ bike-parking-app/components/auth/LoginModal.tsx
@@ -9,7 +9,6 @@ import { createSupabaseBrowserClient } from "@/utils/supabase/browser-client";
 import toast, { Toaster } from "react-hot-toast";
 import { useState } from "react";
 import { Spinner } from "../svgs";
-import styles from './LoginModal.module.css';
 
 const LoginSchema = z.object({
   email: z.string().min(1, "Email is required").email("Invalid email"),
@@ -29,37 +28,22 @@ const LoginModal = () => {
     },
   });
 
-  const onSubmit: SubmitHandler<z.infer<typeof LoginSchema>> = async (values) => {
+  const onSubmit: SubmitHandler<z.infer<typeof LoginSchema>> = async (
+    values
+  ) => {
     setLoading(true);
-    console.log("Received values:", values);  // Check the exact input values
-  
     try {
-      console.log("Checking admin credentials:", values.email, values.password);
-      if (values.email === "bikeparkingbikou@gmail.com" && values.password === "P@ssw0rdP@ssw0rd") {
-        console.log("Redirecting to admin");  // Confirm if it enters this block
-        const result = await router.push("/admin");
-        console.log("Admin redirect result:", result);  // Verify if redirect succeeds
-        return;
-      }
-  
-      console.log("Proceeding with normal login");  // Confirm the flow
       const { error } = await supabase.auth.signInWithPassword(values);
-      if (error && 'message' in error) {
-        console.log("Login error:", error.message);
+      if (error?.message) {
         toast.error(error.message);
+        setLoading(false);
         return;
       } else {
-        console.log("Redirecting to map");
-        await router.push("/map");
+        router.push("/map");
+        router.refresh();
       }
     } catch (error) {
-      if (error instanceof Error) {
-        console.error("Login error:", error);
-        toast.error(`Sign-in error: ${error.message}`);
-      } else {
-        console.error("Unknown error type caught during login:", error);
-        toast.error("An unexpected error occurred.");
-      }
+      toast.error(`Sign-in error: ${error}`);
     } finally {
       setLoading(false);
     }
@@ -76,68 +60,81 @@ const LoginModal = () => {
 
   return (
     <>
-        <Toaster position="top-right" />
-        <div className={`fixed inset-0 z-0 ${styles.backgroundImageFade}`}></div>
-        <div className="fixed inset-x-0 top-0 z-10 flex justify-between items-center p-6 bg-transparent">
-            <h2 className="text-xl font-bold text-white">BikOU</h2>
-            <Link href="/login">
-                <button className="py-2 px-4 rounded-md text-white bg-blue-500 hover:bg-blue-700 font-semibold">
-                    Login
-                </button>
-        </Link>
-      </div>
-      <div className="w-full max-w-sm z-20 mx-auto pt-20">
-        <form
-          className="bg-white shadow-md rounded px-8 py-6"
-          onSubmit={form.handleSubmit(onSubmit)}
-        >
+      <Toaster position="top-right" />
+      <div className="w-full max-w-sm">
+        <form className="bg-white shadow-md rounded px-8 py-6">
           <h1 className="text-center text-2xl pb-3 font-bold">Log In</h1>
-          <div className="mb-4">
-            <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-1">
+          <div className="">
+            <label
+              htmlFor="email"
+              className="block text-gray-700 text-sm font-bold mb-1"
+            >
               Email
             </label>
             <input
               {...form.register("email")}
-              className={`shadow appearance-none border rounded-md w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${form.formState.errors.email ? "border-red-500" : ""}`}
+              className={`shadow appearance-none border rounded-md w-full py-2 px-3 text-gray-700 mb-1 leading-tight focus:outline-none focus:shadow-outline
+              ${form.formState.errors.email ? "border-red-500" : ""}
+              `}
               type="text"
               id="email"
               placeholder="Email"
             />
-            {form.formState.errors.email && (
-              <p className="text-red-500 text-xs italic">{form.formState.errors.email.message}</p>
+            {form.formState.errors.email ? (
+              <p className="text-red-500 text-xs italic">
+                {form.formState.errors.email.message}
+              </p>
+            ) : (
+              <div className="h-4" />
             )}
           </div>
-          <div className="mb-5">
-            <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-1">
+          <div className="mb-5 relative">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-1"
+              htmlFor="password"
+            >
               Password
             </label>
             <input
               {...form.register("password")}
-              className={`shadow appearance-none border rounded-md w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${form.formState.errors.password ? "border-red-500" : ""}`}
-              type="password"
+              className={`shadow appearance-none border rounded-md w-full py-2 px-3 text-gray-700 mb-1 leading-tight focus:outline-none focus:shadow-outline
+              ${form.formState.errors.password ? "border-red-500" : ""}
+              `}
               id="password"
+              type="password"
               placeholder="Password"
             />
-            {form.formState.errors.password && (
-              <p className="text-red-500 text-xs italic">{form.formState.errors.password.message}</p>
-            )}
-            <Link href="#" className="font-bold text-sm text-blue-500 hover:text-blue-800">
-              Forgot Password?
-            </Link>
+            <div className="flex justify-between">
+              {form.formState.errors.password ? (
+                <p className="text-red-500 text-xs italic">
+                  {form.formState.errors.password.message}
+                </p>
+              ) : (
+                <div className="h-5" />
+              )}
+              <Link
+                href="#"
+                className="font-bold text-sm text-blue-500 hover:text-blue-800"
+              >
+                Forgot Password?
+              </Link>
+            </div>
           </div>
           <button
+            onClick={form.handleSubmit(onSubmit)}
             className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md focus:outline-none focus:shadow-outline"
             type="submit"
             disabled={loading}
           >
             {loading ? (
               <div className="flex justify-center">
-                <Spinner className="animate-spin h-6" />
+                <Spinner className="animate-spin h-6"></Spinner>
               </div>
             ) : (
               "Sign In"
             )}
           </button>
+
           <div className="flex justify-center my-4">
             <div className="w-full text-gray-400 flex items-center">
               <div className="border-t-2 w-full" />
@@ -148,18 +145,24 @@ const LoginModal = () => {
             </div>
           </div>
           <button
-            className="flex w-full border-2 py-2 mb-5 rounded-md justify-center items-center font-semibold hover:bg-gray-50"
             onClick={loginWithGoogle}
+            className="flex w-full border-2 py-2 mb-5 rounded-md justify-center items-center font-semibold hover:bg-gray-50"
           >
             <FcGoogle className="h-6 w-6 mr-3" /> Continue with Google
           </button>
           <p className="text-center text-sm font-semibold text-gray-400 mb-3">
-            {"Don't have an account?"}{' '}
-            <Link href="/register" className="text-blue-500 hover:text-blue-800">
+            {`Don't have an account? `}
+            <Link
+              href="/register"
+              className="text-blue-500 hover:text-blue-800 hover:cursor-pointer"
+            >
               Sign up
             </Link>
           </p>
-          <Link href="/map" className="flex justify-center text-sm font-semibold text-gray-400 hover:text-gray-500">
+          <Link
+            href="/map"
+            className="flex justify-center text-sm font-semibold text-gray-400 hover:cursor-pointer hover:text-gray-500"
+          >
             Continue as Guest
           </Link>
         </form>
