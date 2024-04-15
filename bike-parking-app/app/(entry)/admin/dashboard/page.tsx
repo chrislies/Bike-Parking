680f2b083a529b060b76f8a65771831c46b7126f
shion1314 <jiaxinlin200204@gmail.com>
2024-04-15 13:16:45 -0400

    hello

--- bike-parking-app/app/(entry)/admin/dashboard/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import DashboardPage from "@/components/auth/DashboardPage"; // Adjust the import path as needed
-
-const AdminDashboardPage: React.FC = () => {
-  return <DashboardPage />;
-}
-
-export default AdminDashboardPage;
