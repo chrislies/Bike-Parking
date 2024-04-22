23769eaa3247d58eda39f86c29fed41b7d2991d7
Christopher Lai <clai20707@gmail.com>
2024-04-22 15:40:30 -0400

    Improved map features

--- bike-parking-app/components/map/Map.tsx
+++ bike-parking-app/components/map/Map.tsx
@@ -1,31 +1,56 @@
 "use client";
-
 import { MapContainer, TileLayer, LayersControl } from "react-leaflet";
+import { Toaster } from "react-hot-toast";
+import LocateUser from "./LocateUser";
 import MarkerLayer from "./layers/MarkerLayer";
-import MarkerLayer2 from "./layers/MyMarkerLayer";
+import ControlGeocoder from "../LeafletControlGeocoder";
+import { useState } from "react";
+
+const { BaseLayer } = LayersControl;
 
 const RootMap = () => {
-  return (
-    <MapContainer
-      center={[40.71151957593488, -73.88017135962203]}
-      zoom={11}
-      minZoom={3}
-      scrollWheelZoom={true}
-      style={{
-        height: "100vh",
-        width: "100vw",
-      }}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+  const [filter, setFilter] = useState("rack");
 
-      <LayersControl position="topright">
-        <MarkerLayer />
-        <MarkerLayer2 />
-      </LayersControl>
-    </MapContainer>
+  return (
+    <>
+      <Toaster />
+      <MapContainer
+        center={[40.71151957593488, -73.88017135962203]}
+        zoom={11}
+        minZoom={3}
+        scrollWheelZoom={true}
+        style={{
+          height: "100vh",
+          width: "100vw",
+        }}
+        rotate={true}
+        touchRotate={true}
+        rotateControl={{
+          closeOnZeroBearing: false,
+        }}
+      >
+        <LayersControl position="topright">
+          <BaseLayer checked name="Street Layer">
+            <TileLayer
+              maxZoom={24}
+              maxNativeZoom={19}
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              // url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+            />
+          </BaseLayer>
+          <BaseLayer name="Street Layer">
+            <TileLayer
+              maxZoom={24}
+              maxNativeZoom={20}
+              url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+            />
+          </BaseLayer>
+          <MarkerLayer />
+        </LayersControl>
+        <LocateUser />
+        <ControlGeocoder />
+      </MapContainer>
+    </>
   );
 };
 
