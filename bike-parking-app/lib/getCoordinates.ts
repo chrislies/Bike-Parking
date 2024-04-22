b4208eba138b38831957af75548daec145432d6f
Christopher Lai <clai20707@gmail.com>
2024-04-22 15:42:27 -0400

    Refactor fetch

--- bike-parking-app/lib/getCoordinates.ts
+++ bike-parking-app/lib/getCoordinates.ts
@@ -5,63 +5,53 @@ async function getCoordinates(): Promise<MarkerData[] | null> {
     const startRack = window.performance.now();
     let allData: any[] = [];
     let offset = 0;
-    let hasMoreData = true;
     let rackTypes = new Map<string, number>();
-    while (hasMoreData) {
-      const bikeRacksResponse = await fetch(
-        `https://data.cityofnewyork.us/resource/au7q-njtk.json?$limit=50000&$offset=${offset}`
-        // `https://data.cityofnewyork.us/resource/au7q-njtk.json?$limit=1000&$offset=${offset}`
-      );
-      const bikeRacksData: MarkerData[] = await bikeRacksResponse.json();
+    const bikeRacksResponse = await fetch(
+      `https://data.cityofnewyork.us/resource/au7q-njtk.json?$limit=50000&$offset=${offset}`
+      // `https://data.cityofnewyork.us/resource/au7q-njtk.json?$limit=1000&$offset=${offset}`
+    );
+    const bikeRacksData: MarkerData[] = await bikeRacksResponse.json();
 
-      // Count the different types of racks
-      // bikeRacksData.forEach((item: any) => {
-      //   if (!rackTypes.has(item.rack_type)) {
-      //     rackTypes.set(item.rack_type, 1);
-      //   } else {
-      //     const count = rackTypes.get(item.rack_type) || 0;
-      //     rackTypes.set(item.rack_type, count + 1);
-      //   }
-      // });
-      // console.log(rackTypes);
-      //       rack types:
-      // {"Bike Corral" => 759} // same as GFI sled
-      // {"LARGE HOOP" => 11160}
-      // {"WAVE RACK" => 1541}
-      // {"U RACK" => 8612}
-      // {"SMALL HOOP" => 9417}
-      // {"UNDETERMINED" => 55}
-      // {"WAVE RACK (PARKS)" => 18}
-      // {"STAPLE (PARKS)" => 1}
-      // {"DOT SLED (BLACK)" => 7}
-      // {"GFI SLED (SILVER)" => 48}
-      // {"Opal Rack (Parks)" => 18}
+    // Count the different types of racks
+    // bikeRacksData.forEach((item: any) => {
+    //   if (!rackTypes.has(item.rack_type)) {
+    //     rackTypes.set(item.rack_type, 1);
+    //   } else {
+    //     const count = rackTypes.get(item.rack_type) || 0;
+    //     rackTypes.set(item.rack_type, count + 1);
+    //   }
+    // });
+    // console.log(rackTypes);
+    //       rack types:
+    // {"Bike Corral" => 759} // same as GFI sled
+    // {"LARGE HOOP" => 11160}
+    // {"WAVE RACK" => 1541}
+    // {"U RACK" => 8612}
+    // {"SMALL HOOP" => 9417}
+    // {"UNDETERMINED" => 55}
+    // {"WAVE RACK (PARKS)" => 18}
+    // {"STAPLE (PARKS)" => 1}
+    // {"DOT SLED (BLACK)" => 7}
+    // {"GFI SLED (SILVER)" => 48}
+    // {"Opal Rack (Parks)" => 18}
 
-      // Filter bike data by rack_type
-      // const filteredBikeRacksData = bikeRacksData.filter(
-      //   (item: MarkerData) =>
-      //     item.rack_type?.toLowerCase() === "UNDETERMINED".toLowerCase()
-      // );
+    // Filter bike data by rack_type
+    // const filteredBikeRacksData = bikeRacksData.filter(
+    //   (item: MarkerData) =>
+    //     item.rack_type?.toLowerCase() === "UNDETERMINED".toLowerCase()
+    // );
 
-      // Filter bike data by year
-      // const filteredBikeRacksData = bikeRacksData.filter((item: MarkerData) => {
-      //   if (item.date_inst) {
-      //     const year = new Date(item.date_inst).getFullYear();
-      //     if (year === 1900) {
-      //       item.date_inst = "N/A";
-      //     }
-      //   }
-      // });
+    // Filter bike data by year
+    // const filteredBikeRacksData = bikeRacksData.filter((item: MarkerData) => {
+    //   if (item.date_inst) {
+    //     const year = new Date(item.date_inst).getFullYear();
+    //     if (year === 1900) {
+    //       item.date_inst = "N/A";
+    //     }
+    //   }
+    // });
 
-      const combinedData: MarkerData[] = [...bikeRacksData];
-
-      if (combinedData.length === 0) {
-        hasMoreData = false;
-      } else {
-        allData = [...allData, ...combinedData];
-        offset += 50000;
-      }
-    }
+    allData = [...allData, ...bikeRacksData];
 
     const startSign = window.performance.now();
     const streetSignsResponse = await fetch(
@@ -72,16 +62,16 @@ async function getCoordinates(): Promise<MarkerData[] | null> {
       await streetSignsResponse.json();
     const limitData = streetSignsData.street_signs.slice(0, 20000);
     // const limitData = streetSignsData.street_signs.slice(0, 1000);
-    const combinedData: MarkerData[] = [...limitData];
 
-    allData = [...allData, ...combinedData];
+    // allData = [...allData, ...streetSignsData.street_signs];
+    allData = [...allData, ...limitData];
     const endSign = window.performance.now();
 
     // prettier-ignore
     const info: MarkerData[] = allData.map((item) => ({
       x: item.x || item.X,
       y: item.y || item.Y,
-      id: item.site_id ? `R${item.site_id.substring(1)}` : `S.${item.index}`,
+      id: item.site_id ? `R${item.site_id.slice(1)}` : `S.${item.index}`,
       address: item.ifoaddress || `${item.on_street} ${item.from_street} ${item.to_street}`,
       rack_type: item.rack_type,
       date_inst: item.date_inst,
