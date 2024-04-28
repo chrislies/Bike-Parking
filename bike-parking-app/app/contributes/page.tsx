11a89749a29325b6d47a6f040aed46eb3a4d53a6
Xhong0921-rong <hxr3136754148@gmail.com>
2024-04-28 18:19:53 -0400

    Minor updated on contribute page

--- bike-parking-app/app/contributes/page.tsx
+++ bike-parking-app/app/contributes/page.tsx
@@ -15,16 +15,27 @@ export default function ContributionsPage() {
   if (isLoading) {
     return <Loader />;
   }
-
+  
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-3xl font-bold underline">Your Contributions</h1>
-      <div className="mt-10">
-        <Link href="/reports" className="mx-4 p-4 bg-blue-500 text-white rounded hover:bg-blue-700 transition duration-300">
-          Check Your Reports
-        </Link>
-        <Link href="/requests" className="mx-4 p-4 bg-green-500 text-white rounded hover:bg-green-700 transition duration-300">
-          Check Your Requests
+      <h1 className="text-3xl font-bold underline mb-10">Your Contributions</h1>
+      <div className="flex w-full justify-around items-start px-10">
+        <div className="text-center">
+          <Link href="/reports" className="block p-4 bg-blue-500 text-white rounded hover:bg-blue-700 transition duration-300">
+            Check Your Reports
+          </Link>
+          <p className="mt-2 text-2xl text-gray-600">View all the reports you have submitted.</p>
+        </div>
+        <div className="text-center">
+          <Link href="/requests" className="block p-4 bg-green-500 text-white rounded hover:bg-green-700 transition duration-300">
+            Check Your Requests
+          </Link>
+          <p className="mt-2 text-2xl text-gray-600">Manage your pending requests for site changes.</p>
+        </div>
+      </div>
+      <div className="mt-10 w-full flex justify-center">
+        <Link href="/" className="hover:underline font-bold text-lg">
+          {`<-- Go back to map`}
         </Link>
       </div>
     </div>
