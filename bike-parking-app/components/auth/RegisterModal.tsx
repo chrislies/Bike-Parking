6faaefbed9140a0e876f15073241023f33120e49
Christopher Lai <clai20707@gmail.com>
2024-04-30 18:45:51 -0400

    Edit RegisterModal to work within the navbar modals

--- bike-parking-app/components/auth/RegisterModal.tsx
+++ bike-parking-app/components/auth/RegisterModal.tsx
@@ -7,6 +7,8 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/navigation";
 import toast, { Toaster } from "react-hot-toast";
 import { createSupabaseBrowserClient } from "@/utils/supabase/browser-client";
+import { useState } from "react";
+import LoginModal from "./LoginModal";
 
 const RegisterSchema = z
   .object({
@@ -47,9 +49,11 @@ interface FormData {
   confirmPassword: string;
 }
 
-const RegisterModal = () => {
+const RegisterModal: React.FC<{ insideModal: boolean }> = ({ insideModal }) => {
   const supabase = createSupabaseBrowserClient();
   const router = useRouter();
+
+  const [registerView, setRegisterView] = useState(true);
 
   const {
     handleSubmit,
@@ -137,144 +141,166 @@ const RegisterModal = () => {
 
   return (
     <>
-      <Toaster position="top-right" />
-      <div className="w-full max-w-md">
-        <form className="bg-white shadow-md rounded px-8 py-6">
-          <h1 className="text-center text-2xl pb-3 font-bold">Sign Up</h1>
-          <div className="">
-            <label
-              htmlFor="username"
-              className="block text-gray-700 text-sm font-bold mb-1"
+      {registerView ? (
+        <>
+          {!insideModal && <Toaster position="top-right" />}
+          <div className="w-full max-w-md">
+            <form
+              className={`bg-white ${
+                insideModal ? "px-4" : "px-8 py-6 shadow-md rounded"
+              }`}
             >
-              Username
-            </label>
-            <input
-              {...register("username")}
-              className={`shadow appearance-none border rounded-md w-full py-2 px-3 text-gray-700 mb-1 leading-tight focus:outline-none focus:shadow-outline
+              {!insideModal && (
+                <h1 className="text-center text-2xl pb-3 font-bold">Sign Up</h1>
+              )}
+              <div className="">
+                <label
+                  htmlFor="username"
+                  className="block text-gray-700 text-sm font-bold mb-1"
+                >
+                  Username
+                </label>
+                <input
+                  {...register("username")}
+                  className={`shadow appearance-none border rounded-md w-full py-2 px-3 text-gray-700 mb-1 leading-tight focus:outline-none focus:shadow-outline
               ${errors.username ? "border-red-500" : ""}
               `}
-              type="text"
-              id="username"
-              placeholder="Username"
-            />
-            {errors.username ? (
-              <p className="text-red-500 text-xs italic">
-                {errors.username.message}
-              </p>
-            ) : (
-              <div className="h-4" />
-            )}
-          </div>
-          <div className="">
-            <label
-              htmlFor="email"
-              className="block text-gray-700 text-sm font-bold mb-1"
-            >
-              Email
-            </label>
-            <input
-              {...register("email")}
-              className={`shadow appearance-none border rounded-md w-full py-2 px-3 text-gray-700 mb-1 leading-tight focus:outline-none focus:shadow-outline
+                  type="text"
+                  id="username"
+                  placeholder="Username"
+                />
+                {errors.username ? (
+                  <p className="text-red-500 text-xs italic">
+                    {errors.username.message}
+                  </p>
+                ) : (
+                  <div className="h-4" />
+                )}
+              </div>
+              <div className="">
+                <label
+                  htmlFor="email"
+                  className="block text-gray-700 text-sm font-bold mb-1"
+                >
+                  Email
+                </label>
+                <input
+                  {...register("email")}
+                  className={`shadow appearance-none border rounded-md w-full py-2 px-3 text-gray-700 mb-1 leading-tight focus:outline-none focus:shadow-outline
               ${errors.email ? "border-red-500" : ""}
               `}
-              type="text"
-              id="email"
-              placeholder="Email"
-            />
-            {errors.email ? (
-              <p className="text-red-500 text-xs italic">
-                {errors.email.message}
-              </p>
-            ) : (
-              <div className="h-4" />
-            )}
-          </div>
-          <div className="flex gap-4">
-            <div className="flex flex-col">
-              <label
-                className="block text-gray-700 text-sm font-bold mb-1"
-                htmlFor="password"
-              >
-                Password
-              </label>
-              <input
-                {...register("password")}
-                className={`shadow appearance-none border rounded-md w-full py-2 px-3 text-gray-700 mb-1 leading-tight focus:outline-none focus:shadow-outline
+                  type="text"
+                  id="email"
+                  placeholder="Email"
+                />
+                {errors.email ? (
+                  <p className="text-red-500 text-xs italic">
+                    {errors.email.message}
+                  </p>
+                ) : (
+                  <div className="h-4" />
+                )}
+              </div>
+              <div className="flex gap-4">
+                <div className="flex flex-col">
+                  <label
+                    className="block text-gray-700 text-sm font-bold mb-1"
+                    htmlFor="password"
+                  >
+                    Password
+                  </label>
+                  <input
+                    {...register("password")}
+                    className={`shadow appearance-none border rounded-md w-full py-2 px-3 text-gray-700 mb-1 leading-tight focus:outline-none focus:shadow-outline
                 ${errors.password ? "border-red-500" : ""}
                 `}
-                id="password"
-                type="password"
-                placeholder="Password"
-              />
-              {errors.password ? (
-                <p className="text-red-500 text-xs italic">
-                  {errors.password.message}
-                </p>
-              ) : (
-                <div className="h-5" />
-              )}
-            </div>
-            <div className="flex flex-col">
-              <label
-                className="block text-gray-700 text-sm font-bold mb-1"
-                htmlFor="confirmPassword"
-              >
-                Confirm Password
-              </label>
-              <input
-                {...register("confirmPassword")}
-                className={`shadow appearance-none border rounded-md w-full py-2 px-3 text-gray-700 mb-1 leading-tight focus:outline-none focus:shadow-outline
+                    id="password"
+                    type="password"
+                    placeholder="Password"
+                  />
+                  {errors.password ? (
+                    <p className="text-red-500 text-xs italic">
+                      {errors.password.message}
+                    </p>
+                  ) : (
+                    <div className="h-5" />
+                  )}
+                </div>
+                <div className="flex flex-col">
+                  <label
+                    className="block text-gray-700 text-sm font-bold mb-1"
+                    htmlFor="confirmPassword"
+                  >
+                    Confirm Password
+                  </label>
+                  <input
+                    {...register("confirmPassword")}
+                    className={`shadow appearance-none border rounded-md w-full py-2 px-3 text-gray-700 mb-1 leading-tight focus:outline-none focus:shadow-outline
                 ${errors.confirmPassword ? "border-red-500" : ""}
                 `}
-                id="confirmPassword"
-                type="password"
-                placeholder="Confirm Password"
-              />
-              {errors.confirmPassword ? (
-                <p className="text-red-500 text-xs italic mb-1">
-                  {errors.confirmPassword.message}
-                </p>
-              ) : (
-                <div className="h-5" />
-              )}
-            </div>
-          </div>
-          {/* <p className="mb-1 select-none opacity-0 text-red-500 text-xs italic">
+                    id="confirmPassword"
+                    type="password"
+                    placeholder="Confirm Password"
+                  />
+                  {errors.confirmPassword ? (
+                    <p className="text-red-500 text-xs italic mb-1">
+                      {errors.confirmPassword.message}
+                    </p>
+                  ) : (
+                    <div className="h-5" />
+                  )}
+                </div>
+              </div>
+              {/* <p className="mb-1 select-none opacity-0 text-red-500 text-xs italic">
             Please choose a password.
           </p> */}
-          <button
-            onClick={handleSubmit(onSubmit)}
-            className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md focus:outline-none focus:shadow-outline"
-            type="submit"
-          >
-            Sign Up
-          </button>
-          <div className="flex justify-center my-4">
-            <div className="w-full text-gray-400 flex items-center">
-              <div className="border-t-2 w-full" />
-            </div>
-            <p className="text-gray-400 px-4">Or</p>
-            <div className="w-full text-gray-400 flex items-center">
-              <div className="border-t-2 w-full" />
-            </div>
+              <button
+                onClick={handleSubmit(onSubmit)}
+                className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md focus:outline-none focus:shadow-outline"
+                type="submit"
+              >
+                Sign Up
+              </button>
+              <div className="flex justify-center my-4">
+                <div className="w-full text-gray-400 flex items-center">
+                  <div className="border-t-2 w-full" />
+                </div>
+                <p className="text-gray-400 px-4">Or</p>
+                <div className="w-full text-gray-400 flex items-center">
+                  <div className="border-t-2 w-full" />
+                </div>
+              </div>
+              <button
+                onClick={signUpWithGoogle}
+                className="flex w-full border-2 py-2 mb-5 rounded-md justify-center items-center font-semibold hover:bg-gray-50"
+              >
+                <FcGoogle className="h-6 w-6 mr-3" /> Sign up with Google
+              </button>
+              {/* prettier-ignore */}
+              <p className={`text-center text-sm font-semibold text-gray-400 ${insideModal ? null : "mb-3"}`}>
+                {`Already have an account? `}
+                {insideModal ? (
+                  <span
+                    className="text-blue-500 hover:text-blue-800 cursor-pointer"
+                    onClick={() => setRegisterView(false)}
+                  >
+                    Log in
+                  </span>
+                ) : (
+                  <Link
+                    href="/login"
+                    className="text-blue-500 hover:text-blue-800 hover:cursor-pointer"
+                  >
+                    Log in
+                  </Link>
+                )}
+              </p>
+            </form>
           </div>
-          <button
-            onClick={signUpWithGoogle}
-            className="flex w-full border-2 py-2 mb-5 rounded-md justify-center items-center font-semibold hover:bg-gray-50"
-          >
-            <FcGoogle className="h-6 w-6 mr-3" /> Sign up with Google
-          </button>
-          <p className="text-center text-sm font-semibold text-gray-400">
-            Already have an account?{" "}
-            <Link
-              href="/login"
-              className="text-blue-500 hover:text-blue-800 hover:cursor-pointer"
-            >
-              Log in
-            </Link>
-          </p>
-        </form>
-      </div>
+        </>
+      ) : (
+        <LoginModal insideModal={true} />
+      )}
     </>
   );
 };
