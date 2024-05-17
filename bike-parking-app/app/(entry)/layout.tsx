8b80a719e320d89271b31960f9ca577a007ea856
Christopher Lai <clai20707@gmail.com>
2024-05-16 23:58:30 -0400

    Update entry layout

--- bike-parking-app/app/(entry)/layout.tsx
+++ bike-parking-app/app/(entry)/layout.tsx
@@ -1,7 +1,12 @@
+import Navbar from "@/components/navbar/Navbar";
+
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-screen flex items-center justify-center bg-gradient-to-b from-cyan-500 to-blue-500">
-      {children}
+    <div className="h-screen bg-gradient-to-b from-cyan-500 to-blue-500">
+      <Navbar />
+      <div className="h-[80vh] flex justify-center items-center">
+        {children}
+      </div>
     </div>
   );
 };
