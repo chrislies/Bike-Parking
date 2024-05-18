e27d764ce271dc4e6a85edf3fac747fd4d6ebd8a
Christopher Lai <clai20707@gmail.com>
2024-05-18 15:13:45 -0400

    Update

--- bike-parking-app/components/map/MyMarker.tsx
+++ bike-parking-app/components/map/MyMarker.tsx
@@ -1,7 +1,7 @@
 import { getImageSource } from "@/lib/getImageSource";
 import Image from "next/image";
 import L from "leaflet";
-import React, { FC, useCallback, useRef, useState } from "react";
+import React, { FC, useCallback, useEffect, useRef, useState } from "react";
 import { Marker, Popup, useMapEvents } from "react-leaflet";
 import { Bookmark, Directions, NoImage } from "../svgs";
 import { formatDate } from "@/lib/formatDate";
@@ -12,6 +12,10 @@ import queryString from "query-string";
 import axios from "axios";
 import "leaflet-routing-machine";
 import "leaflet-routing-machine/dist/leaflet-routing-machine.css";
+import CommunityReportsModal from "../modals/CommunityReportsModal";
+import BusyComponent from "./BusyComponent";
+import ReportComponent from "./ReportComponent";
+import MyReportComponent from "./MyReportComponent";
 
 interface MyMarkerProps {
   marker: MarkerData;
@@ -38,18 +42,14 @@ const MyMarker: FC<MyMarkerProps> = ({
 
   console.log(`rendering marker`);
   const imageSize = 700;
-  const [isBookmarked, setIsBookmarked] = useState(
-    favoriteLocations.includes(marker.id!)
-  );
+  // prettier-ignore
+  const [isBookmarked, setIsBookmarked] = useState(favoriteLocations.includes(marker.id!));
   const map = useMapEvents({});
   const markerRef = useRef<any>(null);
 
   const handleFlyTo = useCallback(() => {
     const currentZoom = map.getZoom() > 19 ? map.getZoom() : 20;
-    map.flyTo([marker.y!, marker.x!], currentZoom, {
-      animate: true,
-      duration: 1.5,
-    });
+    map.setView([marker.y!, marker.x!], currentZoom);
   }, [map]);
 
   const handleSaveLocation = useCallback(async () => {
@@ -107,114 +107,124 @@ const MyMarker: FC<MyMarkerProps> = ({
   }, [markerRef, queryIcon, isBookmarked]);
 
   return (
-    <Marker
-      key={marker.id}
-      ref={markerRef}
-      // prettier-ignore
-      icon={isBookmarked ? favoriteIcon : marker.type === "rack" ? rackIcon : signIcon}
-      position={[marker.y!, marker.x!]}
-      eventHandlers={{
-        click: handleFlyTo,
-      }}
-    >
-      <Popup minWidth={170}>
-        {/* <Popup minWidth={200}> */}
-        <div className="popup-rack flex flex-col">
-          <div className="flex flex-col font-bold">
-            <p className="side_id !m-0 !p-0 text-base">{marker.id}</p>
-            <p className="rack_type !m-0 !p-0 text-base">{marker.rack_type}</p>
-            {/* TODO: Add # of reports here */}
-            {/* {marker.site_id && <ReportComponent siteId={marker.site_id} x={marker.x} y={marker.y} />} */}
-            {/* <DeleteComponent x={marker.x} y={marker.y} site_id={marker.site_id} /> */}
-            {/* <BusyComponent x={marker.x!} y={marker.y!} /> */}
-          </div>
-          <div className="my-1 flex justify-center items-center select-none pointer-events-none">
-            {marker.rack_type && getImageSource(marker.rack_type) ? (
-              <Image
-                className="rounded-md shadow"
-                src={getImageSource(marker.rack_type)}
-                alt={marker.rack_type}
-                height={imageSize}
-                width={imageSize}
-              />
-            ) : marker.type === "sign" ? (
-              <Image
-                className="rounded-md shadow"
-                src={"/images/streetsign.jpg"}
-                alt={"street sign"}
-                height={imageSize}
-                width={imageSize}
-              />
-            ) : (
-              <div className="flex flex-col w-full items-center bg-slate-200 border-[1px] border-slate-300 rounded-lg p-3">
-                <NoImage />
-                <p className="!p-0 !m-0 text-xs">No image available</p>
+    <>
+      <Marker
+        key={marker.id}
+        ref={markerRef}
+        // prettier-ignore
+        icon={isBookmarked ? favoriteIcon : marker.type === "rack" ? rackIcon : signIcon}
+        position={[marker.y!, marker.x!]}
+        eventHandlers={{
+          click: handleFlyTo,
+        }}
+      >
+        <Popup minWidth={170}>
+          <div className="popup-rack flex flex-col">
+            <div className="flex flex-col font-bold">
+              <div className="flex items-center">
+                <p className="rack_type !m-0 !p-0 text-base font-extrabold font-sans">
+                  {marker.rack_type ? marker.rack_type : "Street Sign"}
+                </p>
               </div>
-            )}
-          </div>
-          {/* prettier-ignore */}
-          <p className={`ifo_address text-center ${marker.type === 'rack' ? 'text-base overflow-x-auto' : 'max-w-[170px] text-sm tracking-tight'} !p-0 !m-0`}>
-            {marker.address}
-          </p>
-          <div className="flex flex-col gap-2 mt-1 mb-3">
-            <button
-              onClick={handleSaveLocation}
-              title="Save Location"
-              aria-label="Save Location"
-              aria-disabled="false"
-              className="flex text-sm font-bold justify-center items-center w-full border-[1px] rounded-3xl border-slate-300 bg-slate-200 hover:bg-slate-300"
-            >
-              <Bookmark
-                className={`h-7 w-7 hover:cursor-pointer fill-transparent ${
-                  isBookmarked ? "fill-yellow-300" : "fill-transparent"
-                }`}
-              />
-              Save
-            </button>
-            <button
-              onClick={() => {
-                handleGetDirections(marker);
-              }}
-              title="Directions"
-              aria-label="Directions"
-              aria-disabled={isCalculatingRoute}
-              disabled={isCalculatingRoute}
-              className="flex text-sm font-bold justify-center items-center w-full border-[1px] rounded-3xl border-blue-600 hover:shadow-lg gap-1 text-white bg-blue-600 disabled:cursor-not-allowed"
-            >
-              <Directions
-                className={`h-7 w-7 hover:cursor-pointer items-end`}
-              />
-              {isCalculatingRoute ? "Calculating..." : "Directions"}
-            </button>
-
-            {/* Delete Button */}
-            {/* <button
-              onClick={() => handleDeleteRequest(marker)}
-              title="Delete"
-              aria-label="Delete"
-              aria-disabled="false"
-              className="flex text-sm font-bold justify-center items-center w-full border-[1px] rounded-3xl border-blue-600 hover:shadow-lg gap-1 text-white bg-blue-600"
-            >
-              <Directions
-                className={`h-7 w-7 hover:cursor-pointer items-end`}
-              />
-              DeleteRequest
-            </button> */}
-          </div>
-          {marker.type === "rack" ? (
-            <div className="flex justify-between items-end">
-              <p className="date_installed italic text-xs !m-0 !p-0">
-                {`Date Installed: `}
-                {marker.date_inst &&
-                new Date(marker.date_inst).getFullYear() === 1900
-                  ? "N/A"
-                  : formatDate(marker.date_inst!)}
-              </p>
+              {marker.id && (
+                <MyReportComponent
+                  siteId={marker.id}
+                  x={marker.x}
+                  y={marker.y}
+                  rack_type={marker.rack_type}
+                  address={marker.address}
+                />
+              )}
+              {/* <DeleteComponent x={marker.x} y={marker.y} site_id={marker.site_id} /> */}
+              <BusyComponent x={marker.x!} y={marker.y!} />
             </div>
-          ) : null}
-        </div>
-      </Popup>
-    </Marker>
+            <div className="my-1 flex justify-center items-center select-none pointer-events-none">
+              {marker.rack_type && getImageSource(marker.rack_type) ? (
+                <Image
+                  className="rounded-md shadow"
+                  src={getImageSource(marker.rack_type)}
+                  alt={marker.rack_type}
+                  height={imageSize}
+                  width={imageSize}
+                />
+              ) : marker.type === "sign" ? (
+                <Image
+                  className="rounded-md shadow"
+                  src={"/images/streetsign.jpg"}
+                  alt={"street sign"}
+                  height={imageSize}
+                  width={imageSize}
+                />
+              ) : (
+                <div className="flex flex-col w-full items-center bg-slate-200 border-[1px] border-slate-300 rounded-lg p-3">
+                  <NoImage />
+                  <p className="!p-0 !m-0 text-xs">No image available</p>
+                </div>
+              )}
+            </div>
+            {/* prettier-ignore */}
+            <p className={`ifo_address text-center ${marker.type === 'rack' ? 'text-base overflow-x-auto' : 'max-w-[170px] text-sm tracking-tight'} !p-0 !m-0`}>
+              {marker.address}
+            </p>
+            <div className="flex flex-col gap-2 mt-1 mb-3">
+              <button
+                onClick={handleSaveLocation}
+                title="Save Location"
+                aria-label="Save Location"
+                aria-disabled="false"
+                className="flex text-sm font-bold justify-center items-center w-full border-[1px] rounded-3xl border-slate-300 bg-slate-200 hover:bg-slate-300"
+              >
+                <Bookmark
+                  className={`h-7 w-7 hover:cursor-pointer fill-transparent ${
+                    isBookmarked ? "fill-yellow-300" : "fill-transparent"
+                  }`}
+                />
+                Save
+              </button>
+              <button
+                onClick={() => {
+                  handleGetDirections(marker);
+                }}
+                title="Directions"
+                aria-label="Directions"
+                aria-disabled={isCalculatingRoute}
+                disabled={isCalculatingRoute}
+                className="flex text-sm font-bold justify-center items-center w-full border-[1px] rounded-3xl border-blue-600 hover:shadow-lg gap-1 text-white bg-blue-600 disabled:cursor-not-allowed"
+              >
+                <Directions
+                  className={`h-7 w-7 hover:cursor-pointer items-end`}
+                />
+                {isCalculatingRoute ? "Calculating..." : "Directions"}
+              </button>
+              {/* Delete Button */}
+              {/* <button
+                onClick={() => handleDeleteRequest(marker)}
+                title="Delete"
+                aria-label="Delete"
+                aria-disabled="false"
+                className="flex text-sm font-bold justify-center items-center w-full border-[1px] rounded-3xl border-blue-600 hover:shadow-lg gap-1 text-white bg-blue-600"
+              >
+                <Directions
+                  className={`h-7 w-7 hover:cursor-pointer items-end`}
+                />
+                DeleteRequest
+              </button> */}
+            </div>
+            {marker.type === "rack" ? (
+              <div className="flex justify-between items-end">
+                <p className="date_installed italic text-xs !m-0 !p-0">
+                  {`Date Installed: `}
+                  {marker.date_inst &&
+                  new Date(marker.date_inst).getFullYear() === 1900
+                    ? "N/A"
+                    : formatDate(marker.date_inst!)}
+                </p>
+              </div>
+            ) : null}
+          </div>
+        </Popup>
+      </Marker>
+    </>
   );
 };
 
