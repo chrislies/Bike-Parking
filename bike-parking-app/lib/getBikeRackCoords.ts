a11b9e85094407f24f44920d48466f88f49f2185
Christopher Lai <clai20707@gmail.com>
2024-11-03 15:30:28 -0500

    Update data fetch

--- bike-parking-app/lib/getBikeRackCoords.ts
+++ bike-parking-app/lib/getBikeRackCoords.ts
@@ -3,25 +3,25 @@ async function getBikeRackCoords(): Promise<MarkerData[] | null> {
     let allData: any[] = [];
     let offset = 0;
     const bikeRacksResponse = await fetch(
-      // `https://data.cityofnewyork.us/resource/au7q-njtk.json?$limit=50000&$offset=${offset}`
-      `https://data.cityofnewyork.us/resource/au7q-njtk.json?$limit=1000&$offset=${offset}`
+      `https://data.cityofnewyork.us/resource/592z-n7dk.json?$limit=50000&$offset=${offset}`
+      // `https://data.cityofnewyork.us/resource/592z-n7dk.json?$limit=1000&$offset=${offset}`
     );
     const bikeRacksData: MarkerData[] = await bikeRacksResponse.json();
     allData = [...allData, ...bikeRacksData];
 
     // prettier-ignore
-    const info: MarkerData[] = allData.map((item) => ({
-      x: item.x,
-      y: item.y,
+    allData = allData.map((item) => ({
+      x: item.x || item.longitude,
+      y: item.y || item.latitude,
       id: item.site_id && `R${item.site_id.slice(1)}`,
       address: item.ifoaddress,
-      rack_type: item.rack_type,
+      rack_type: item.racktype,
       date_inst: item.date_inst,
       favorite: false,
       type: "rack",
     }));
 
-    return info.length > 0 ? info : null;
+    return allData.length > 0 ? allData : null;
   } catch (error) {
     console.error(error);
     throw new Error("Failed to fetch data");
