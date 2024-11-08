53de961d20853c1d1b5c5a46051eff66442206de
Christopher Lai <clai20707@gmail.com>
2024-11-08 17:29:09 -0500

    Add contact form to page

--- bike-parking-app/app/(entry)/contact/page.tsx
+++ bike-parking-app/app/(entry)/contact/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import React, { useState } from "react";
+import { Mail, Spinner } from "@/components/svgs";
 import styles from "../../css/contactPage.module.css";
+import "../../css/contactPage.css";
 
 interface TeamMemberProps {
   name: string;
@@ -25,7 +27,6 @@ const TeamMemberCard: React.FC<TeamMemberProps> = ({
   return (
     <div className={styles.column} onClick={() => setIsOpen(!isOpen)}>
       <div className={styles.card}>
-        {/* <img src="/contact-us-bg.jpg" alt={name} className={styles.width100} /> */}
         <div className={styles.container}>
           <h2>{name}</h2>
           <p className={styles.title}>{title}</p>
@@ -71,15 +72,122 @@ const ContactPage: React.FC = () => {
       bio: "Xuanrong oversees our quality assurance processes, implementing rigorous testing methods to ensure our application meets high standards.",
     },
   ];
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [errors, setErrors] = useState({
+    nameError: "",
+    emailError: "",
+    messageError: "",
+  });
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setLoading(true); // Set loading to true during submission
+
+    try {
+      const res = await fetch("api/contact", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          message,
+        }),
+      });
+
+      const { msg, success } = await res.json();
+      // console.log("Server response:", { msg, success }); // Log the server response
+
+      if (success) {
+        // console.log("Form submitted successfully"); // Log successful form submission
+        setName("");
+        setEmail("");
+        setMessage("");
+        setErrors({
+          nameError: "",
+          emailError: "",
+          messageError: "",
+        });
+        setSuccess(true);
+      } else {
+        // console.log("Form submission failed", msg); // Log failed form submission and error messages
+        // Map error messages to input fields
+        const errorMap: Record<string, string | string[]> = {};
+        msg.forEach((err: string | string[]) => {
+          if (err.includes("Name")) {
+            errorMap.nameError = err;
+            const nameElement = document.querySelector(".name");
+            if (nameElement) {
+              nameElement.scrollIntoView({
+                behavior: "smooth",
+                block: "center",
+                inline: "nearest",
+              });
+            }
+          } else if (err.includes("valid name")) {
+            errorMap.nameError = err;
+            const nameElement = document.querySelector(".name");
+            if (nameElement) {
+              nameElement.scrollIntoView({
+                behavior: "smooth",
+                block: "center",
+                inline: "nearest",
+              });
+            }
+          } else if (err.includes("Email")) {
+            errorMap.emailError = err;
+            const emailElement = document.querySelector(".email");
+            if (emailElement) {
+              emailElement.scrollIntoView({
+                behavior: "smooth",
+                block: "center",
+                inline: "nearest",
+              });
+            }
+          } else if (err.includes("Phone")) {
+            errorMap.phoneError = err;
+          } else if (err.includes("Message")) {
+            errorMap.messageError = err;
+          } else {
+            errorMap.emailError = err;
+            const emailElement = document.querySelector(".email");
+            if (emailElement) {
+              emailElement.scrollIntoView({
+                behavior: "smooth",
+                block: "center",
+                inline: "nearest",
+              });
+            }
+          }
+        });
+        // console.log("New errors:", { ...errors, ...errorMap }); // Log the new errors
+
+        setErrors((prevErrors) => ({
+          ...prevErrors, // Preserve existing values
+          ...errorMap, // Update with new values
+        }));
+        setSuccess(false);
+      }
+    } catch (error) {
+      console.log("Error submitting form:", error);
+    } finally {
+      setLoading(false); // Set loading to false after submission
+    }
+  };
 
   return (
     <>
-      <main className={styles.page} style={{ paddingTop: "100px" }}>
-        <section className={`${styles.aboutSection} bg-white/80`} id="about">
+      {/* <main className={styles.page} style={{ paddingTop: "100px" }}>
+        <section className={`${styles.aboutSection}`} id="about">
           <h1>Contact Us</h1>
           <p>
             The Bike Parking app is designed to make it easy for anyone who
-            rides a bike or scooter to find a good place to park.
+            rides a bike or scooter to find a <em>good</em> place to park.
           </p>
         </section>
         <section id="team">
@@ -88,8 +196,112 @@ const ContactPage: React.FC = () => {
               <TeamMemberCard key={index} {...member} />
             ))}
           </div>
-        </section>
+        </section> 
       </main>
+        */}
+      <section className="w-screen">
+        <form
+          onSubmit={handleSubmit}
+          className="pt-8 pb-10 flex flex-col items-center gap-5 2xl:px-[200px] max-2xl:px-[200px] max-xl:px-[100px] max-lg:px-8"
+        >
+          <div className="name">
+            <label htmlFor="name">
+              Name<span className="text-red-500">*</span>
+            </label>
+            <input
+              onChange={(event) => {
+                setName(event.target.value);
+                // Clear the error when the user starts typing
+                setErrors((prevErrors) => ({ ...prevErrors, nameError: "" }));
+              }}
+              value={name}
+              type="text"
+              id="name"
+              placeholder=""
+              className={`${
+                errors.nameError ? "border-red-500" : "border-slate-300"
+              }`}
+            />
+            <span id="nameError" className="text-red-600">
+              {errors.nameError}
+            </span>
+          </div>
+          <div className="email">
+            <label htmlFor="email">
+              Email<span className="text-red-500">*</span>
+            </label>
+            <input
+              onChange={(event) => {
+                setEmail(event.target.value);
+                // Clear the error when the user starts typing
+                setErrors((prevErrors) => ({
+                  ...prevErrors,
+                  emailError: "",
+                }));
+              }}
+              value={email}
+              type="text"
+              id="email"
+              placeholder=""
+              className={`${
+                errors.emailError ? "border-red-500" : "border-slate-300"
+              }`}
+            />
+            <span id="emailError" className="text-red-600">
+              {errors.emailError}
+            </span>
+          </div>
+          <div className="message">
+            <label htmlFor="message">
+              Message<span className="text-red-500">*</span>
+            </label>
+            <textarea
+              onChange={(event) => {
+                setMessage(event.target.value);
+                // Clear the error when the user starts typing
+                setErrors((prevErrors) => ({
+                  ...prevErrors,
+                  messageError: "",
+                }));
+              }}
+              value={message}
+              style={{ minHeight: "128px", resize: "vertical" }}
+              id="message"
+              placeholder=""
+              className={`${
+                errors.messageError ? "border-red-500" : "border-slate-300"
+              }`}
+            ></textarea>
+            <span id="messageError" className="text-red-600">
+              {errors.messageError}
+            </span>
+          </div>
+          <button
+            className="bg-green-600 hover:bg-green-500 rounded p-3 w-[150px] text-white font-bold"
+            type="submit"
+            disabled={loading}
+          >
+            {loading ? (
+              <div className="flex flex-row">
+                <Spinner className="animate-spin h-6 mr-3"></Spinner>
+                Sending...
+              </div>
+            ) : (
+              "Send"
+            )}
+          </button>
+          <div className="2xl:h-5 max-2xl:h-5 max-sm:h-[30px]">
+            {success && (
+              <div
+                className={`bg-slate-100 text-green-800 text-lg px-5 py-2`}
+                key="success"
+              >
+                Message sent successfully.
+              </div>
+            )}
+          </div>
+        </form>
+      </section>
     </>
   );
 };
