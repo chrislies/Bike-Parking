ad18e345b81bdbd4bb3249daec6f03fb65abd76c
Christopher Lai <clai20707@gmail.com>
2024-11-09 12:30:06 -0500

    Add ScrollToTop component

--- bike-parking-app/app/contact/layout.tsx
+++ bike-parking-app/app/contact/layout.tsx
@@ -1,8 +1,12 @@
+"use client";
+
 import Navbar from "@/components/navbar/Navbar";
+import ScrollToTop from "@/components/ScrollToTop";
 
 const ContactLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
+      <ScrollToTop />
       <Navbar />
       <div>{children}</div>
     </div>
