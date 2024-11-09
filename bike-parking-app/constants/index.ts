95a6e457e138c4249fde5641bdba29f89147d714
Christopher Lai <clai20707@gmail.com>
2024-11-09 17:03:23 -0500

    Include constants

---
+++ bike-parking-app/constants/index.ts
@@ -0,0 +1,66 @@
+export const FEATURES = [
+  {
+    feature: "Save a spot",
+    desc: "Registered members can save up to 5 spots for quick access.",
+    imgSrc: "/images/feature_favorites.png",
+  },
+  {
+    feature: "Report a spot",
+    desc: "Registered members can report an existing spot for problems.",
+    imgSrc: "/images/feature_report.png",
+  },
+  {
+    feature: "Add a new spot",
+    desc: "Registered members can request to add a new spot to the map.",
+    imgSrc: "/images/feature_add-spot.png",
+  },
+  {
+    feature: "Request to delete a spot",
+    desc: "Registered members can request to delete an existing spot on the map.",
+    imgSrc: "/images/feature_delete-spot.png",
+  },
+  {
+    feature: "Manage contributions",
+    desc: "Registered members can view and delete their previous map contributions including reports and user-added spots.",
+    imgSrc: "/images/feature_contributions.png",
+  },
+];
+
+export const TEAM_MEMBERS = [
+  {
+    name: "Christopher Lai",
+    title: "Full Stack Developer",
+    description: "Focuses on user experience and design.",
+    email: "clai20707@gmail.com",
+    github: "chrislies",
+    linkedin: "christwofour",
+    bio: "Christopher is our lead UI/UX designer. With a keen eye for design and user experience, he ensures all interfaces are intuitive and engaging.",
+  },
+  {
+    name: "Jiaxin Lin",
+    title: "Backend Developer",
+    description: "Expert in Node.js and databases.",
+    email: "jiaxinlin200204@gmail.com",
+    github: "Shion1314",
+    linkedin: "jiaxinl1314",
+    bio: "Jiaxin is a backend specialist with extensive experience in server-side languages and database management. He ensures our data systems are efficient and secure.",
+  },
+  {
+    name: "Xuanrong Hong",
+    title: "Backend Developer",
+    description: "Guarantees product quality and reliability.",
+    email: "hxr3136754148@gmail.com",
+    github: "Xhong0921-rong",
+    linkedin: "xuanrong-hong-157b82240",
+    bio: "Xuanrong oversees our quality assurance processes, implementing rigorous testing methods to ensure our application meets high standards.",
+  },
+  {
+    name: "Eric Ma",
+    title: "Frontend Developer",
+    description: "Specializes in React and CSS.",
+    email: "ericma25.em@gmail.com",
+    github: "em-cp",
+    linkedin: "",
+    bio: "Eric has over 10 years of experience in front-end technologies including React, Angular, and Vue.js. He is passionate about building accessible, user-friendly web applications.",
+  },
+];
