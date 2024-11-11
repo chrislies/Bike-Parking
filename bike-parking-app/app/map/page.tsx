cd0acd58eea39a593dbd76fbb7254414d157e289
Christopher Lai <clai20707@gmail.com>
2024-11-11 15:55:54 -0500

    Remove unused imports

--- bike-parking-app/app/map/page.tsx
+++ bike-parking-app/app/map/page.tsx
@@ -1,20 +1,14 @@
 "use client";
-import Loader from "@/components/Loader";
-import dynamic from "next/dynamic";
 import { Toaster } from "react-hot-toast";
 import SuperClusterMap from "@/components/map/SuperClusterMap";
 import { QueryClient, QueryClientProvider } from "react-query";
-const ClientMap = dynamic(() => import("../../components/map/Map"), {
-  loading: () => <Loader />,
-  ssr: false,
-});
+
 const queryClient = new QueryClient();
 
 export default function MapPage() {
   return (
     <div className="container">
       <Toaster />
-      {/* <ClientMap /> */}
       <QueryClientProvider client={queryClient}>
         <SuperClusterMap />
       </QueryClientProvider>
