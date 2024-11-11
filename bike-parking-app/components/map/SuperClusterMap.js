60f1564d11ac056105693fff68279d22c76effc2
Christopher Lai <clai20707@gmail.com>
2024-11-11 15:03:13 -0500

    Create SuperClusterMap

---
+++ bike-parking-app/components/map/SuperClusterMap.js
@@ -0,0 +1,32 @@
+"use client";
+
+import { useQuery } from "react-query";
+import { MapContainer, TileLayer } from "react-leaflet";
+import getCoordinates from "@/lib/getCoordinates";
+import ShowSpots from "./layers/SuperClusterLayer";
+
+export default function SuperClusterMap() {
+  const { isLoading, error, data } = useQuery("repoData", getCoordinates, {
+    staleTime: 5 * 60 * 1000, // Keep the data fresh for 5 minutes
+    cacheTime: 10 * 60 * 1000, // Cache the data for 10 minutes
+    refetchOnWindowFocus: false, // Prevent refetch on window/tab focus
+  });
+
+  if (isLoading) return <div>Loading...</div>;
+
+  if (error) return <div>An error has occurred: {error.message}</div>;
+
+  return (
+    <MapContainer
+      center={[40.71151957593488, -73.88017135962203]}
+      zoom={13}
+      style={{ height: "100vh", width: "100vw" }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <ShowSpots data={data} />
+    </MapContainer>
+  );
+}
