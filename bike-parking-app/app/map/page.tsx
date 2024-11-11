5f5bbb2134dc8dea6b6c2f3333d365cc1737b6d8
Christopher Lai <clai20707@gmail.com>
2024-11-11 14:58:00 -0500

    Render  SuperClusterMap

--- bike-parking-app/app/map/page.tsx
+++ bike-parking-app/app/map/page.tsx
@@ -1,18 +1,23 @@
+"use client";
 import Loader from "@/components/Loader";
-import LeafletMap from "@/components/map/testMap";
 import dynamic from "next/dynamic";
 import { Toaster } from "react-hot-toast";
+import SuperClusterMap from "@/components/map/SuperClusterMap";
+import { QueryClient, QueryClientProvider } from "react-query";
 const ClientMap = dynamic(() => import("../../components/map/Map"), {
   loading: () => <Loader />,
   ssr: false,
 });
+const queryClient = new QueryClient();
 
 export default function MapPage() {
   return (
     <div className="container">
       <Toaster />
-      <ClientMap />
-      {/* <LeafletMap /> */}
+      {/* <ClientMap /> */}
+      <QueryClientProvider client={queryClient}>
+        <SuperClusterMap />
+      </QueryClientProvider>
     </div>
   );
 }
