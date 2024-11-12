5d77fa51483e9568750f5a40cbe2dc2322e8d89a
chrislies <clai20707@gmail.com>
2024-11-11 20:24:43 -0500

    Add conditional checks for recipient list

--- bike-parking-app/lib/nodemailer.js
+++ bike-parking-app/lib/nodemailer.js
@@ -2,10 +2,11 @@ import nodemailer from "nodemailer";
 
 const email = process.env.EMAIL;
 const pass = process.env.EMAIL_PASS;
-let recipients = process.env.EMAIL_RECIPIENTS.includes(",")
-  ? process.env.EMAIL_RECIPIENTS.split(",")
-  : [process.env.EMAIL_RECIPIENTS];
-
+let recipients = process.env.EMAIL_RECIPIENTS
+  ? process.env.EMAIL_RECIPIENTS.includes(",")
+    ? process.env.EMAIL_RECIPIENTS.split(",")
+    : [process.env.EMAIL_RECIPIENTS]
+  : [];
 export const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
