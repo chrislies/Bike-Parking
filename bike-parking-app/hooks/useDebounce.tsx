09bcb2f95ac167fd095ced1eec95b5f7d49d7c7e
Christopher Lai <clai20707@gmail.com>
2024-11-17 16:30:08 -0500

    Remove unnecessary page and hooks

--- bike-parking-app/hooks/useDebounce.tsx
+++
@@ -1,9 +0,0 @@
-export const debounce = (func: Function, delay: number) => {
-  let timeoutId: NodeJS.Timeout;
-  return (...args: any[]) => {
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => {
-      func(...args);
-    }, delay);
-  };
-};
