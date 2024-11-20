9f1e1a4afd6e6351151cfec9d6cb036be76ff371
Christopher Lai <clai20707@gmail.com>
2024-11-19 19:15:02 -0500

    Add shelterIcon to MAP_ICONS dictionary

--- bike-parking-app/constants/mapIcons.ts
+++ bike-parking-app/constants/mapIcons.ts
@@ -7,6 +7,12 @@ export const MAP_ICONS = {
     iconAnchor: [20, 30],
     popupAnchor: [3, -16],
   }),
+  shelterIcon: new L.Icon({
+    iconUrl: "/svgs/ShelterMarker.svg",
+    iconSize: [45, 50],
+    iconAnchor: [20, 30],
+    popupAnchor: [3, -16],
+  }),
   signIcon: new L.Icon({
     iconUrl: "/svgs/SignMarker.svg",
     iconSize: [45, 50],
