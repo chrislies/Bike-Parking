b6d587772801943eca9f28003ec7fa52fb7f5f5b
Christopher Lai <clai20707@gmail.com>
2025-03-15 22:36:06 -0400

    Add Vercel Analytics

--- bike-parking-app/app/layout.tsx
+++ bike-parking-app/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import "leaflet/dist/leaflet.css";
@@ -6,18 +7,16 @@ import "leaflet/dist/leaflet.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Bike Parking",
-  description: "Bike Parking App",
+  title: "LockySpots",
+  description:
+    "A community-driven interactive map that helps you find accessible bike and scooter parking spots, from bike racks to street signs.",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <Analytics />
     </html>
   );
 }
